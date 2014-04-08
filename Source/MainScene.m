fe3e419f7c8bc3bb525c89180dc5d21eb17b2d1c
Ricardo Rubio <rickarubio@gmail.com>
2014-04-08 16:07:04 -0700

    adds obstacles

--- Source/MainScene.m
+++ Source/MainScene.m
@@ -8,6 +8,8 @@
 
 #import "MainScene.h"
 static const CGFloat scrollSpeed = 80.f;
+static const CGFloat firstObstaclePosition = 280.f;
+static const CGFloat distanceBetweenObstacles = 160.f;
 
 @implementation MainScene {
     CCSprite *_hero;
@@ -16,11 +18,29 @@ static const CGFloat scrollSpeed = 80.f;
     CCNode *_ground2;
     NSArray *_grounds;
     NSTimeInterval _sinceTouch;
+    NSMutableArray *_obstacles;
+}
+
+- (void)spawnNewObstacle {
+    CCNode *previousObstacle = [_obstacles lastObject];
+    CGFloat previousObstacleXPosition = previousObstacle.position.x;
+    if(!previousObstacle) {
+        // this is the first obstacle
+        previousObstacleXPosition = firstObstaclePosition;
+    }
+    CCNode *obstacle = [CCBReader load:@"Obstacle"];
+    obstacle.position = ccp(previousObstacleXPosition + distanceBetweenObstacles, 0);
+    [_physicsNode addChild:obstacle];
+    [_obstacles addObject:obstacle];
 }
 
 - (void)didLoadFromCCB {
     _grounds = @[_ground1, _ground2];
     self.userInteractionEnabled = true;
+    _obstacles = [NSMutableArray array];
+    [self spawnNewObstacle];
+    [self spawnNewObstacle];
+    [self spawnNewObstacle];
 }
 
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
